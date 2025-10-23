8a7afe2eb014417963725107ff0723b71ce66168
fantasystar94 <aniseed_3@naver.com>
2025-10-23 11:33:24 +0900

    feat: 키오스크 Lv3 Kiosk 클래스 생성.

--- src/Lv_3/Kiosk.java
+++ src/Lv_3/Kiosk.java
@@ -1,4 +1,65 @@
 package Lv_3;
+
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Scanner;
+
 // Kiosk: 프로그램 순서 및 흐름 제어를 담당하는 클래스
 public class Kiosk {
+    //속
+    List<MenuItem> list = new ArrayList<>();
+    //생
+    Kiosk(List<MenuItem> list) {
+        this.list = list;
+    }
+    //기
+    public void start() {
+        Scanner sc = new Scanner(System.in);
+        String menuTemplate = "";
+
+        for(int i = 0; i<list.size(); i++) {
+            String prdName = "";
+            String blank ="";
+            int blankArea = 15 - list.get(i).name.toString().length();
+            for(int j = 0;j<blankArea;j++){
+                blank += " ";
+                prdName = list.get(i).name.toString()+blank;
+            }
+            menuTemplate += (i+1)+". "+prdName+"|"+" W"+list.get(i).price+" | "+list.get(i).description+"\n";
+        }
+        menuTemplate += "0. 종료      | 종료";
+        while(true){
+            System.out.println("[ SHAKESHACK MENU ]");
+            System.out.println(menuTemplate);
+            try{
+                int input = sc.nextInt();
+                switch(input) {
+                    case 1 :
+                        System.out.println(list.get(input-1).name.toString()+" (이/가) 주문 되었습니다");
+                        break;
+                    case 2 :
+                        System.out.println(list.get(input-1).name.toString()+" (이/가) 주문 되었습니다");
+                        break;
+                    case 3 :
+                        System.out.println(list.get(input-1).name.toString()+" (이/가) 주문 되었습니다");
+                        break;
+                    case 4 :
+                        System.out.println(list.get(input-1).name.toString()+" (이/가) 주문 되었습니다");
+                        break;
+                    case 0 :
+                        System.out.println("프로그램을 종료합니다.");
+                        return;
+                    default:
+                        System.out.println("주문번호만 정확히 입력해주세요.");
+                        break;
+                }
+            }
+            catch(InputMismatchException e){
+                System.out.println("주문번호만 입력해주세요");
+                sc.nextLine();
+            }
+        }
+    }
+
 }
